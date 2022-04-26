d14b0132db6a1e709877b0bbfa77adc29f6125e9
Drikus Roor <drikusroor@gmail.com>
2022-04-26 10:47:58 +0200

    Install ant design

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,8 @@
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
-import App from './App.vue'
+import { createApp } from "vue";
+import { createPinia } from "pinia";
+import Antd from "ant-design-vue";
+import App from "./App.vue";
 
-const app = createApp(App)
+import "ant-design-vue/dist/antd.css";
 
-app.use(createPinia())
-
-app.mount('#app')
+createApp(App).use(createPinia()).use(Antd).mount("#app");
