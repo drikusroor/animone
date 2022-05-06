9686fd5b6233b82e77a53ff3820576dd264bad35
Drikus Roor <drikusroor@gmail.com>
2022-05-06 08:39:36 +0200

    Wip on resize elements

--- src/components/RenderElement.vue
+++ src/components/RenderElement.vue
@@ -6,17 +6,21 @@ import { Button } from "ant-design-vue";
 import { PlayCircleOutlined, StopOutlined } from "@ant-design/icons-vue";
 import { Animation } from "../models/Animation";
 
+import VueResizable from "vue-resizable";
+
 export default {
   props: ["element", "index"],
   components: {
     Button,
     PlayCircleOutlined,
     StopOutlined,
+    VueResizable,
   },
   data() {
     return {
       isPlaying: false,
       animationTimeout: null,
+      dragSelector: ".drag-container",
     };
   },
   computed: {
@@ -86,6 +90,26 @@ export default {
       }
       this.elementStore.selectElement(this.index);
     },
+    onResize: function (x, y, width, height) {
+      this.x = x;
+      this.y = y;
+      this.width = width;
+      this.height = height;
+    },
+    onDragEnd({ left, top }) {
+      if (this.animationStore.selectedStep) {
+        this.animationStore.selectedStep.styleString += `\nleft: ${left}px;\ntop: ${top}px;`;
+      } else if (this.animationStore.selectedAnimation) {
+        this.animationStore.selectedAnimation.styleString += `\nleft: ${left}px;\ntop: ${top}px;`;
+      } else {
+        this.elementStore.elements[
+          this.index
+        ].styleString += `\nleft: ${left}px;\ntop: ${top}px;`;
+      }
+    },
+    onResizeMove(e, left, top, width, height) {
+      console.log(e, left, top, width, height);
+    },
   },
   setup() {
     const animationStore = useAnimationStore();
@@ -97,12 +121,17 @@ export default {
 </script>
 
 <template>
-  <div
+  <vue-resizable
     @click="selectElement()"
     class="element"
     ref="element"
+    :id="`element${element.id}`"
     :class="{ 'element--selected': selected, [element.className]: true }"
     :style="elementStyle"
+    @drag:end="onDragEnd"
+    @resize:end="onResizeEnd"
+    dragSelector=".drag-selector"
+    :fitContent="true"
   >
     <Button @click="playAnimation" :disabled="isPlaying">
       <PlayCircleOutlined />
@@ -110,7 +139,11 @@ export default {
     <Button @click="stopAnimation" :disabled="!isPlaying">
       <StopOutlined />
     </Button>
-  </div>
+
+    <div class="resizable-content">
+      <div class="drag-selector"></div>
+    </div>
+  </vue-resizable>
 </template>
 
 <style scoped>
@@ -120,6 +153,7 @@ export default {
   cursor: pointer;
   transition: filter 0.15s ease-in-out;
   background: #ccc;
+  position: relative;
 }
 .element--selected {
   border: 2px dashed #000;
@@ -136,5 +170,20 @@ export default {
   border-radius: 5px;
   bottom: 0.25em;
   margin: 0 auto;
+}
+.resizable-content {
+  top: 0;
+  height: 100%;
+  position: absolute;
+  width: 100%;
+}
+
+.drag-selector {
+  width: 80%;
+  height: 80%;
+  cursor: pointer;
+}
+.drag-selector:hover {
+  background-color: rgba(255, 255, 255, 0.25);
 }
 </style>
--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,10 @@ import { createPinia } from "pinia";
 import Antd from "ant-design-vue";
 import App from "./App.vue";
 
+
 import "ant-design-vue/dist/antd.css";
 
-createApp(App).use(createPinia()).use(Antd).mount("#app");
+createApp(App)
+  .use(createPinia())
+  .use(Antd)
+  .mount("#app");
